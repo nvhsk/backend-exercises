547076c4d57f4d3b018d7769b98bfd1705aaa3e0
Iryna <119431403+nvhsk@users.noreply.github.com>
2023-09-18 11:38:45 +0200

    done: Routes challenge

---
+++ backend-basics_routes/index.js
@@ -0,0 +1,6 @@
+import { server } from "./server.js";
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running 🐡`);
+});
---
+++ backend-basics_routes/server.js
@@ -0,0 +1,15 @@
+import { createServer } from "node:http";
+
+export const server = createServer((request, response) => {
+  if (request.url === "/api/fish/1") {
+    response.statusCode = 200;
+    response.end("first fish 🐠");
+  }
+  if (request.url === "/api/fish/2") {
+    response.statusCode = 200;
+    response.end("second fish 🐡");
+  } else {
+    response.statusCode = 404;
+    response.end("no fish found");
+  }
+});
