92af8640ea308a484a9d20604f6019e73a06b2c1
Iryna <119431403+nvhsk@users.noreply.github.com>
2023-09-18 12:16:54 +0200

    fix

--- backend-basics_chance/index.js
+++ backend-basics_chance/index.js
@@ -1,4 +1,6 @@
 import { server } from "./server.js";
 
 const port = 4000;
-server.listen(port);
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port} 👾`);
+});
