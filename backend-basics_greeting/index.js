5f2e6f928211fb1b512e98191cefc4a0fc707a57
Iryna <119431403+nvhsk@users.noreply.github.com>
2023-09-18 12:23:06 +0200

    add UTF-8 charset

--- backend-basics_greeting/index.js
+++ backend-basics_greeting/index.js
@@ -2,5 +2,5 @@ import { server } from "./server.js";
 
 const port = 8000;
 server.listen(port, () => {
-  console.log(`Server is running at localhost:${port} 👾`);
+  console.log(`Server is running at http://localhost:${port} 👾`);
 });
--- backend-basics_greeting/server.js
+++ backend-basics_greeting/server.js
@@ -1,5 +1,7 @@
 import { createServer } from "node:http";
 
 export const server = createServer((request, response) => {
-  response.end("Hello, Iri!");
+  response.setHeader("Content-Type", "text/plain; charset=utf-8");
+
+  response.end("Hello, Iri! 👩🏻‍💻");
 });
